5a618e94e9962155305e2bc5102e351cf22aa049
Max7537 <godgakil@gmail.com>
2023-05-09 18:31:07 +0500

    NBA Project with routing

---
+++ src/Players/Players.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Players() {
+    useEffect(()=>{
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'd8fc1d850dmsha5a022c62c586f6p106cbcjsnf6e37576149e',
+                'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+            }
+        }
+
+        fetch('https://free-nba.p.rapidapi.com/players?page=0&per_page=25', options)
+        .then(response => response.json())
+        .then(response => setPlayers(response.data))
+        .catch(err => console.log(err))
+    })
+
+    const [players, setPlayers] = useState([]);
+  return (
+    <div className='container'>
+      { players.map((el)=>{
+        return <p>Имя игрока: {el.first_name}</p>
+      }) }
+    </div>
+  )
+}
